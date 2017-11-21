017a0769fa03e9009006412ef2e05362476719b2
beppek <beppe@live.se>
2017-11-21 20:17:08 +0100

    blog (maybe not needed)

--- src/main/java/clustering/blogs/Blog.java
+++ src/main/java/clustering/blogs/Blog.java
@@ -1,24 +1,7 @@
 package clustering.blogs;
 
-import java.util.HashMap;
-import java.util.Map;
-
-class Blog {
-    private String title;
-    private Map<String, Integer> wordFrequencies = new HashMap<>();
+class Blog extends Article {
     Blog(String title) {
-        this.title = title;
-    }
-
-    String getTitle() {
-        return title;
-    }
-
-    Map<String, Integer> getWordFrequencies() {
-        return wordFrequencies;
-    }
-
-    void addWord(String word, Integer frequency) {
-        wordFrequencies.put(word, frequency);
+        super(title);
     }
 }
