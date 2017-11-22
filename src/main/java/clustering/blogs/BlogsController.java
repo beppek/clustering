4b1db446cfd78360d22251ebac8fd012fccae441
beppek <beppe@live.se>
2017-11-22 23:37:37 +0100

    creates hierarchy

--- src/main/java/clustering/blogs/BlogsController.java
+++ src/main/java/clustering/blogs/BlogsController.java
@@ -3,7 +3,12 @@ package clustering.blogs;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import clustering.clusters.Cluster;
+import clustering.clusters.Hierarchy;
+import clustering.clusters.KMeans;
 import clustering.common.Article;
+import clustering.common.TreeBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +16,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/blogs")
 public class BlogsController {
 
-    @RequestMapping("/k-clusters")
-    public List<Article> kClusters() {
+    @RequestMapping("/kmeans")
+    public String kClusters() {
         Blogs blogs = new Blogs();
-        List<Article> items = new ArrayList<>();
         try {
-            items = blogs.readBlogData();
+            blogs.readBlogData();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return items;
+        KMeans kMeans = new KMeans(blogs.getBlogs());
+        kMeans.generate(4);
+        return "Not yet";
+    }
+
+    @RequestMapping("/hierarchy")
+    public String hierarchy() {
+        Blogs blogs = new Blogs();
+        try {
+            blogs.readBlogData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Hierarchy h = new Hierarchy(blogs.getBlogs());
+        h.generate();
+        Cluster root = h.getClusters().get(0);
+        TreeBuilder tb = new TreeBuilder(root);
+        String htmlTree = tb.buildHTMLTree();
+        return htmlTree;
     }
 }
