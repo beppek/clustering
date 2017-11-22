90a5ec7128d26255b6fe9c9d5093553e959e1348
beppek <beppe@live.se>
2017-11-22 23:37:21 +0100

    build JTree and HTML tree

--- src/main/java/clustering/common/TreeBuilder.java
+++ src/main/java/clustering/common/TreeBuilder.java
@@ -1,14 +1,60 @@
 package clustering.common;
 
 import clustering.clusters.Cluster;
-import clustering.clusters.Hierarchy;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TreeBuilder {
-    public JTree buildJTree(Hierarchy h) {
-        Cluster root = h.getClusters().get(0);
+    private List<String> html;
+    private Cluster root;
+
+    public TreeBuilder(Cluster root) {
+        this.root = root;
+    }
+
+    public String buildHTMLTree() {
+        html = new ArrayList<>();
+        html.add("<ul>");
+        html.add("</ul>");
+
+        addHTMLNodes(1, root);
+        StringBuilder sb = new StringBuilder();
+        for (String s : html) {
+            sb.append(s).append("\n");
+        }
+        return sb.toString();
+    }
+
+    private void addHTMLNodes(int i, Cluster c) {
+        if (c.hasRight()) {
+            String art = c.getRight().toString();
+            if (art.equals("")) {
+                html.add(i, "<li><ul>");
+                html.add(i + 1, "</ul></li>");
+            } else {
+                art = art.replaceAll("\"", "'");
+                html.add(i, "<li data-jstree='{\"disabled\":true}'>" + art + "</li>");
+            }
+            addHTMLNodes(i + 1, c.getRight());
+        }
+
+        if (c.hasLeft()) {
+            String art = c.getLeft().toString();
+            if (art.equals("")) {
+                html.add(i, "<li><ul>");
+                html.add(i + 1, "</ul></li>");
+            } else {
+                art = art.replaceAll("\"", "'");
+                html.add(i, "<li data-jstree='{\"disabled\":true}'>" + art + "</li>");
+            }
+            addHTMLNodes(i + 1, c.getLeft());
+        }
+    }
+
+    public JTree buildJTree() {
         DefaultMutableTreeNode top = new DefaultMutableTreeNode(root.toString());
         jTreeAddNodes(top, root);
         JTree tree = new JTree(top);
