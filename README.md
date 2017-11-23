8dbe0278e0a7b18e0aeb8de0fa0de7aa44b2ee9f
beppek <beppe@live.se>
2017-11-23 23:17:01 +0100

    add react

---
+++ README.md
@@ -0,0 +1,14 @@
+# Clustering
+This is a java application that clusters blog and wikipedia data by K-means and hierarchy with Pearson similarity.
+Start by running the java application from your IDE of choice. You can also run the application using maven from the project root.
+
+Run the front end using node by opening a terminal in the root of the project. Then run
+```shell
+npm install
+```
+
+Next time you start the application you can start it from the project root with
+```shell
+npm start
+```
+This will start a react app which can be accessed in the browser at [localhost:3000](http://localhost:3000) if it doesn't automatically open.
---
+++ src/main/javascript/clustering/src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Request from 'superagent';
+import './App.css';
+
+import ReactHTMLConverter from 'react-html-converter';
+
+const apiRoot = 'http://localhost:8080/';
+
+class Test extends React.Component {
+  render() {
+      return <div>{this.props.text}</div>;
+  }
+}
+
+const converter = new ReactHTMLConverter();
+converter.registerComponent('test', Test);
+
+class App extends Component {
+
+  state = {
+    content: "To get started, select information to cluster from the menu above, then select the cluster type you wish to display."
+  };
+
+  handleClick = (path) => {
+    console.log(path);
+    Request
+      .get(apiRoot+path)
+      .end((err, res) => {
+        if (!err) {
+          this.setState({
+            content: res.text
+          });
+        }
+      });
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="menu">
+            <a className="menu-item" onClick={() => this.handleClick('blogs/kmeans')}>Blogs: K-Means</a>
+            <a className="menu-item" onClick={() => this.handleClick('blogs/hierarchy')}>Blogs: Hierarchy</a>
+            <a className="menu-item" onClick={() => this.handleClick('wiki/blogs')}>Wikipedia: K-Means</a>
+            <a className="menu-item" onClick={() => this.handleClick('wiki/hierarchy')}>Wikipedia: Hierarchy</a>
+          </div>
+        </header>
+        <div className="App-intro">
+          {converter.convert(this.state.content)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
