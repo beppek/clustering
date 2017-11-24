99f592a5ed83961447fab5251abdea2bb6e21820
beppek <beppe@live.se>
2017-11-24 01:26:56 +0100

    creates wikipages

--- src/main/java/clustering/wiki/WikiPages.java
+++ src/main/java/clustering/wiki/WikiPages.java
@@ -1,4 +1,28 @@
 package clustering.wiki;
 
+import clustering.common.Article;
+import clustering.common.FileHandler;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class WikiPages {
+    private List<Article> pages = new ArrayList<Article>();
+    private FileHandler fileHandler = new FileHandler();
+
+    void createWordFrequencyFile() throws IOException {
+        fileHandler.createWikiDataFile();
+    }
+
+    /**
+     * Reads the wikidata.txt file
+     * */
+    void readWikiData() throws IOException {
+        pages = fileHandler.readFileData("data/wikidata.txt");
+    }
+
+    List<Article> getPages() {
+        return pages;
+    }
 }
