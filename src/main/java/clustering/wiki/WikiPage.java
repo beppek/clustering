0805a7b8bf31296d9d645ec2a0c42c0010ee9015
beppek <beppe@live.se>
2017-11-24 13:41:46 +0100

    handles word frequency of wikipage

--- src/main/java/clustering/wiki/WikiPage.java
+++ src/main/java/clustering/wiki/WikiPage.java
@@ -1,4 +1,28 @@
 package clustering.wiki;
 
-public class WikiPage {
+import clustering.common.Article;
+import clustering.common.WordFrequencyMap;
+
+
+public class WikiPage extends Article {
+    private WordFrequencyMap words;
+
+    public WikiPage(String title) {
+        super(title);
+        words = new WordFrequencyMap();
+    }
+
+    public void addWord(String word, int count) {
+        words.addWord(word, count);
+    }
+
+    public int get(String word) {
+        int count;
+        try {
+            count = words.get(word);
+        } catch (NullPointerException e) {
+            return 0;
+        }
+        return count;
+    }
 }
